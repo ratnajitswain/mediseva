4d56030cd540a46ea66d17008d6a435a414e3e11
Ratnajit Swain <ratnajitswain@gmail.com>
2021-06-09 16:25:12 +0530

    first commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
             </div>`
             $('#doctData').append(docts)
             })
-            $('.owl-carousel').owlCarousel({
+            $('#doctData').owlCarousel({
                 loop:true,
                 margin:10,
                 responsiveClass:true,
@@ -53,5 +53,145 @@ $(document).ready(function(){
             })
         }
     )
-    
+    $.getJSON("./data/blogs.json", function(data){
+        $('#blogContent').html('')
+        console.log(data)
+        data.forEach((d)=>{  
+            d.blogid = "blog"+Math.floor(Math.random() * 1000);
+            if(d.type=="video"){
+                var videoBlogs = `<div  style="padding: 16px">
+
+                <a class="btn btn-play video-popup" id="${d.blogid}" href="${d.url}">
+                     
+                  
+                <img height="155px" class="borderRound" src="images/thumbnail/${d.thumbnail}">
+                <div
+                    style="height: 160px;padding: 12px 0px 18px 12px;background-color: #fff;border-radius: 0px 0px 10px 10px;">
+                    <h5 style="color: #464646;">${d.title}</h5>
+                    <p>${d.Description}</p>
+                    
+                </div>
+            </a>
+            </div>`
+            $('#blogContent').append(videoBlogs)
+            
+            $('#'+d.blogid).magnificPopup({
+                type: 'iframe',
+                mainClass: 'mfp-with-zoom',
+                preload: [0,2],
+                iframe: {
+                    markup: '<div class="mfp-iframe-scaler">' +
+                        '<div class="mfp-close"></div>' +
+                        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                        '</div>',
+                    patterns: {
+                        youtube: {
+                            index: 'youtube.com/',
+                            id: 'v=',
+                            src: '//www.youtube.com/embed/%id%?autoplay=1'
+                        }
+                    },
+                    srcAction: 'iframe_src'
+                }
+        
+            });
+
+            }
+
+
+           
+
+
+
+        })
+
+        $('#blogContent').owlCarousel({
+            loop:true,
+            margin:10,
+            responsiveClass:true,
+            autoplay:true,
+            autoplayTimeout: 2000,
+                    autoplayspeed:2000,
+                    nav:true,
+                    navText : ['<i class="fas fa-angle-double-left" aria-hidden="true"></i>','<i class="fas fa-angle-double-right" aria-hidden="true"></i>'],
+            responsive:{
+                0:{
+                    items:1,
+                    
+                    center: true,
+                    loop:false,
+                   
+                },
+                600:{
+                    items:3,
+                   
+                    center: true,
+                    loop:false,
+                   
+                },
+                1000:{
+                    items:4,
+                   
+                    loop:false
+                    
+                }
+            }
+        })
+   
+
+})
+
+
+$.getJSON("./data/gallery.json", function(data){
+    $('#galleryimage').html('')
+    data.forEach((d)=>{  
+        d.imgid = "blog"+Math.floor(Math.random() * 1000);
+            var images = `<div  style="padding: 16px">
+            <a id="${d.imgid}" href="images/blogs/${d.url}">
+            <img  style="width: 100%;" height="155px" src="images/blogs/${d.url}">
+           <a>
+        </div>`
+        $('#galleryimage').append(images)
+        
+        $('#'+d.imgid).magnificPopup({type:'image'
+        });
+
+    })
+
+    $('#galleryimage').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        autoplay:true,
+        autoplayTimeout: 2000,
+                autoplayspeed:2000,
+                nav:true,
+                navText : ['<i class="fas fa-angle-double-left" aria-hidden="true"></i>','<i class="fas fa-angle-double-right" aria-hidden="true"></i>'],
+        responsive:{
+            0:{
+                items:1,
+                
+                center: true,
+                loop:false,
+               
+            },
+            600:{
+                items:3,
+               
+                center: true,
+                loop:false,
+               
+            },
+            1000:{
+                items:4,
+               
+                loop:false
+                
+            }
+        }
+    })
+
+
+})
   });
+
